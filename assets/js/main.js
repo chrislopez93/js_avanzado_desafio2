81bb08d7c59328721b9b62477dc54337f63c8cf0
chrislopez93 <sinretel@gmail.com>
2023-07-26 00:26:08 -0400

    Tarea 2 finalizada - mejorar captura de valores enteros y añadir visibility: hidden

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,17 @@ function margen(){
 }
 //Accede al elemento con imagenId y añade la clase imagenClass con classList.toggle
 
+function verCard(){
+    const kirby = document.querySelector('#kirby').value;
+    const zelda = document.querySelector('#zelda').value;
+    const mario = document.querySelector('#mario').value;
+    const suma = Number(kirby) + Number(mario) + Number(zelda);
+        document.querySelector('.seleccion_stickers').innerHTML = " " + suma;
+    const alerta = document.querySelector('#alerta_stickers');
+    if(suma >= 11){
+      alerta.style.display= 'block'
+    }else{
+        alerta.style.display= 'none'
+
+    }
+}
