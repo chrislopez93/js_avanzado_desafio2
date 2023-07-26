009073b2b43408bc687c0535109541de664d9d17
chrislopez93 <sinretel@gmail.com>
2023-07-26 02:53:41 -0400

    Tarea 3 finalizada

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@ function margen(){
     const margen = document.getElementById("imagenId");
     margen.classList.toggle("imagenClass");
 }
-//Accede al elemento con imagenId y añade la clase imagenClass con classList.toggle
+//Accede al elemento con imagenId y añade la clase imagenClass con classList.toggle. 
+//El metodo toggle() no se puede utilizar dentro de una condición if.
 
 function verCard(){
     const kirby = document.querySelector('#kirby').value;
@@ -19,4 +20,19 @@ function verCard(){
     }
 }
 
-// ¡Atención! Haz sobrepasado los 10 stickers permitidos
+function verkey(){
+    const varizquierda = document.querySelector('#izquierda').value;
+    const varcentro = document.querySelector('#centro').value;
+    const varderecha = document.querySelector('#derecha').value;
+
+    if(varizquierda == 9 && varcentro == 1 && varderecha == 1){
+        const pkey = document.querySelector('.pkey');
+        pkey.innerHTML = "Contraseña 1 correcta"; 
+    }else if (varizquierda == 7 && varcentro == 1 && varderecha == 4){
+        const pkey = document.querySelector('.pkey');
+        pkey.innerHTML = "Contraseña 2 correcta"; 
+    }else{
+        const pkey = document.querySelector('.pkey');
+        pkey.innerHTML = "Contraseña incorrecta"; 
+    }
+}
